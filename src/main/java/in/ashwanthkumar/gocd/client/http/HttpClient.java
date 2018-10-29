cd92191a2f70d9ff8dad097dcc71c747e4e43ccb
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2018-10-29 10:04:22 +0530

    Making read + socket timeouts configurable

--- src/main/java/in/ashwanthkumar/gocd/client/http/HttpClient.java
+++ src/main/java/in/ashwanthkumar/gocd/client/http/HttpClient.java
@@ -15,25 +15,32 @@ import java.net.Proxy;
 public class HttpClient {
     private static Logger LOG = LoggerFactory.getLogger(HttpClient.class);
 
-    // TODO - Would we ever want to make this configurable?
-    private final static int SOCKET_TIMEOUT = 600 * 1000;
-    private final static int READ_TIMEOUT = 600 * 1000;
+    private final static int DEFAULT_SOCKET_TIMEOUT = 600 * 1000;
+    private final static int DEFAULT_READ_TIMEOUT = 600 * 1000;
 
     // for tests
     private String mockResponse;
 
     private final HttpRequestFactory requestFactory;
+    private final int socketTimeout;
+    private final int readTimeout;
 
     public HttpClient(String username, String password, Proxy proxy) {
+        this(username, password, proxy, DEFAULT_SOCKET_TIMEOUT, DEFAULT_READ_TIMEOUT);
+    }
+
+    public HttpClient(String username, String password) {
+        this(username, password, null);
+    }
+
+    public HttpClient(String username, String password, Proxy proxy, int socketTimeout, int readTimeout) {
         NetHttpTransport.Builder builder = new NetHttpTransport.Builder();
         if (proxy != null) {
             builder.setProxy(proxy);
         }
         requestFactory = builder.build().createRequestFactory(new BasicAuthentication(username, password));
-    }
-
-    public HttpClient(String username, String password) {
-        this(username, password, null);
+        this.socketTimeout = socketTimeout;
+        this.readTimeout = readTimeout;
     }
 
     // for testing only
@@ -73,8 +80,8 @@ public class HttpClient {
     public HttpRequest invokeGET(String url) throws IOException {
         LOG.debug("Hitting " + url);
         return requestFactory.buildGetRequest(new GenericUrl(url))
-                .setConnectTimeout(SOCKET_TIMEOUT)
-                .setReadTimeout(READ_TIMEOUT);
+                .setConnectTimeout(this.socketTimeout)
+                .setReadTimeout(this.readTimeout);
     }
 
 
